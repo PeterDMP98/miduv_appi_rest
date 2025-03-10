57ac268e5544dd0f0a21f86cf60b79df654a338a
PeterDMP98 <peter.dmp.ca@gmail.com>
2025-03-10 16:58:43 -0500

    cors agregado

--- app.js
+++ app.js
@@ -7,25 +7,33 @@ const { validateMovie, validarPartialMovie } = require('./schemas/movies');
 const app = express();
 app.use(express.json())
 
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:5500',
+            'http://localhost:1234',
+            'http://localhost:8080',
+            'https://movies.com',
+            'https://midu.com',
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}))
+
 app.disable('x-powered-by'); // desabilita la cabecera x-powered-by
 
-const ACCEPTED_ORIGINS = [
-    'http://localhost:5500',
-    'http://localhost:1234',
-    'http://localhost:3000',
-    'https://myapp.com',
-    'https://movies.com',
-    'https://midu.dev.com',
-    'http://127.0.0.1:5500',
-    'https://apirestmidudev.netlify.app/'
-]
 
 // todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
-    const origin = req.header('origin')
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-    }
 
     const {genre} = req.query
     if (genre) {
@@ -75,6 +83,22 @@ app.post('/movies', (req, res) => {
     res.status(201).json(newMovie)
 })
 
+
+app.delete('/movies/:id', (req, res) => {
+
+    const {id} = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if(movieIndex === -1) return res.status(404).json({message: 'Movie not found'})
+
+    movies.splice(movieIndex, 1)
+
+    
+    return res.json({message: 'Movie deleted'})
+
+})
+
+
 app.patch('/movies/:id', (req, res) => {
     const result = validarPartialMovie(req.body)
     
@@ -95,23 +119,6 @@ app.patch('/movies/:id', (req, res) => {
     return res.json(updateMovie)
 })
 
-app.delete('/movies/:id', (req, res) => {
-    const origin = req.header('origin')
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin)
-    }
-
-    const {id} = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-
-    if(movieIndex === -1) return res.status(404).json({message: 'Movie not found'})
-
-    movies.splice(movieIndex, 1)
-
-    
-    return res.json({message: 'Movie deleted'})
-
-})
 
 app.options('/movies/:id', (req, res) => {
     const origin = req.header('origin')
